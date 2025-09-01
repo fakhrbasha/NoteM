9d2dfacb282e272eb06dfbae410599c2ebfb2454
fakhrbasha <fakhrbasha8@gmail.com>
2025-09-01 15:37:40 +0300

    fix:update notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 export default function NotFound() {
-    return (
-        <div>
-            <h1>NotFound</h1>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-gray-800">404</h1>
+        <p className="mt-4 text-xl text-gray-600">Page Not Found</p>
+        <p className="mt-2 text-gray-500">
+          Sorry, the page you are looking for doesn’t exist or has been moved.
+        </p>
+        <a
+          href="/"
+          className="mt-6 inline-block px-6 py-3 bg-[#1ebbcc]  text-white font-medium rounded-lg shadow hover:bg-[#0d353a]  transition"
+        >
+          Go Back Home
+        </a>
+      </div>
+    </div>
+  );
 }
